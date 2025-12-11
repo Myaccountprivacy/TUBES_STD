97e2f0f1c27a4cadd8d859113e9b656a782c0e12
derpyzx <62276138+Myaccountprivacy@users.noreply.github.com>
2025-12-11 10:33:27 +0700

    modified:   bootcamp.cpp

--- bootcamp.cpp
+++ bootcamp.cpp
@@ -36,7 +36,19 @@ void insertBootcamp(List &L, adrBootcamp p) {
 
 // (b) & (d) Menambahkan sesi ke bootcamp tertentu
 void addSesiToBootcamp(List &L, string judulBootcamp, adrSesi C) {
-
+    adrBootcamp p=findBootcamp(L,judulBootcamp);
+    if (p!=nullptr) {
+        if (p->firstSesi==nullptr) {
+            p->firstSesi=C;
+        } else {
+            adrSesi q=p->firstSesi;
+            while (q->next!=nullptr) {
+                q=q->next;
+            }
+            q->next=C;
+            C->prev=q;
+        }
+    }
 }
 
 // (c) Mencari bootcamp tertentu berdasarkan judul
@@ -53,13 +65,23 @@ adrBootcamp findBootcamp(List L, string judul) {
 
 // (e) Menampilkan sesi berdasarkan bootcamp tertentu
 void showSesiByBootcamp(List L, string judulBootcamp) {
-
+    adrBootcamp bc=findBootcamp(L,judulBootcamp);
+    adrSesi p=bc->firstSesi;
+    if (bc!=nullptr) {
+        cout<<"Sesi pada bootcamp "<<judulBootcamp<<":\n";
+        while (p!=nullptr) {
+            cout<<"- "<<p->info.namaSesi<<" | Durasi: "<<p->info.durasi<<" jam | Kesulitan: "<<p->info.kesulitan<<"\n";
+            p=p->next;
+        }
+    } else {
+        cout<<"Bootcamp dengan nama "<<judulBootcamp<<" tidak ditemukan.\n";
+    }
 }
 
 // (f) Menghapus bootcamp beserta seluruh sesinya
 // Hati-hati: Harus hapus semua anak dulu baru bapaknya
 void deleteBootcamp(List &L, string judulBootcamp) {
-
+    
 }
 
 // (g) Menghapus sesi tertentu dari bootcamp tertentu
@@ -69,38 +91,67 @@ void deleteSesi(List &L, string judulBootcamp, string namaSesi) {
 
 // (h) Menampilkan seluruh bootcamp beserta sesinya
 void showAll(List L) {
-
+    adrBootcamp p=L.first;
+    while (p!=nullptr) {
+        cout<<"Bootcamp: "<<p->info.judul<<" | Penyelenggara: "<<p->info.penulis<<" | Kategori: "<<p->info.kategori<<"\n";
+        adrSesi q=p->firstSesi;
+        if (q==nullptr) {
+            cout<<"  (Tidak ada sesi)\n";
+        } else {
+            cout<<"  Sesi:\n";
+            while (q!=nullptr) {
+                cout<<"  - "<<q->info.namaSesi<<" | Durasi: "<<q->info.durasi<<" jam | Kesulitan: "<<q->info.kesulitan<<"\n";
+                q=q->next;
+            }
+        }
+        p=p->next;
+    }
 }
 
 // (i) Menghitung jumlah sesi dalam bootcamp tertentu
 int countSesi(List L, string judulBootcamp) {
     int tally=0;
+    adrBootcamp bc=findBootcamp(L, judulBootcamp);
     adrSesi p=L.first->firstSesi;
-    while (p!=nullptr) {
-        tally++;
-        p=p->next;
+    if (bc!=nullptr) {
+        while (p!=nullptr) {
+            tally++;
+            p=p->next;
+        }
     }
     return tally;
 }
 
 // (j) Menampilkan bootcamp dengan sesi terbanyak dan paling sedikit
 void showMinMaxSesi(List L) { // might be wrong
-    adrSesi p=L.first->firstSesi;
-    adrSesi top=L.first->firstSesi;
-    adrSesi bottom=L.first->firstSesi;
+    //adrBootcamp bc=findBootcamp(L, L.first->info.judul);
+    adrBootcamp p=L.first;
+    adrBootcamp top=p;
+    adrBootcamp bottom=p;
     while (p!=nullptr) {
-        if (countSesi(L, p->info.namaSesi) > countSesi(L, top->info.namaSesi)) {
+        if (countSesi(L,p->info.judul)>countSesi(L,top->info.judul)) {
             top=p;
         }
-        if (countSesi(L, p->info.namaSesi) < countSesi(L, bottom->info.namaSesi)) {
+        if (countSesi(L,p->info.judul)<countSesi(L,bottom->info.judul)) {
             bottom=p;
         }
         p=p->next;
     }
-    cout<<"Bootcamp dengan sesi terbanyak: "<<top->info.namaSesi<<" ("<<countSesi(L, top->info.namaSesi)<<" sesi)"<<endl;
+    cout<<"Bootcamp dengan sesi terbanyak: "<<top->info.judul<<" ("<<countSesi(L, top->info.judul)<<" sesi)"<<endl;
+    cout<<"Bootcamp dengan sesi paling sedikit: "<<bottom->info.judul<<" ("<<countSesi(L, bottom->info.judul)<<" sesi)"<<endl;
 }
 
 // Helper untuk menu (k)
 void printMenu() {
-
+    cout<<"=== Menu Bootcamp Management ===\n";
+    cout<<"1. Tambah Bootcamp\n";
+    cout<<"2. Tambah Sesi ke Bootcamp\n";
+    cout<<"3. Cari Bootcamp berdasarkan judul\n";
+    cout<<"4. Tampilkan Sesi berdasarkan Bootcamp\n";
+    cout<<"5. Hapus Bootcamp beserta Sesi-nya\n";
+    cout<<"6. Hapus Sesi dari Bootcamp\n";
+    cout<<"7. Tampilkan Semua Bootcamp beserta Sesi-nya\n";
+    cout<<"8. Hitung Jumlah Sesi dalam Bootcamp\n";
+    cout<<"9. Tampilkan Bootcamp dengan Sesi Terbanyak dan Paling Sedikit\n";
+    cout<<"10. Keluar\n";
 }
