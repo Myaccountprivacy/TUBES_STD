d3c0fe09816643c55044f4d44b0d03846af744af
EmirRasyadxx <emirrasyad2007@gmail.com>
2025-12-11 13:37:08 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main() {
     infoSesi dataS;
     string judulCari, namaSesiCari;
     adrBootcamp pB;
-    adrSesi;
+    adrSesi pS;
 
     pilihan = -1;
 
-    while (pilihan != 0){
+    while (pilihan != 10){
         printMenu();
         cout << "Masukkan pilihan  :";
         cin >> pilihan;
@@ -35,7 +35,60 @@ int main() {
             cout << "--- Tambah Sesi ---\n";
             cout << "Masukkan ke Bootcamp (judul): "; cin >> judulCari;
 
-            if (findBootcamp_103012400164(L))
+            if (findBootcamp_103012400164(L, judulCari) != nullptr ) {
+                cout << "Nama sesi   : "; cin >> dataS.namaSesi;
+                cout << "Durasi (jam): "; cin >> dataS.durasi;
+                cout << "kesulitan:  : "; cin >> dataS.kesulitan; 
+                
+                pS= createElmSesi_103012400164(dataS);
+                addSesiToBootcamp_103012400206(L, judulCari, pS);
+                
+            } else if (pilihan == 3){
+                string judulCari2;
+                cout << "--- Cari Bootcamp ---\n";
+                cout << "Nama Bootcamp  :  "; cin >> judulCari2;
+                adrBootcamp px=findBootcamp_103012400164(L,judulCari2);
+                if (px!=nullptr) {
+                    cout<<"Ditemukan, "<<px->info.judul<<"\n";
+                } else {
+                    cout<<"Bootcamp tidak ditemukan.\n";
+                }
+                
+            } else if (pilihan == 4){
+                cout << "--- Tampilkan Sesi Bootcamp ---\n";
+                string judulCari3;
+                cout << "Bootcamp yang dicari : "; cin >> judulCari3;
+                showSesiByBootcamp_103012400164(L, judulCari3);
+                
+            } else if (pilihan == 5){
+                cout << "--- Hapus Bootcamp beserta sesinya ---\n";
+                string judulCari4;
+                cout << "Bootcamp yang dicari : "; cin >> judulCari4;
+                deleteBootcamp_103012400206(L, judulCari4);
+
+                
+            } else if (pilihan == 6){
+                cout << "--- Hapus Sesi dari Bootcamp ---\n";
+                string judulCari5;
+                string cariSesi;
+                cout << "Bootcamp yang dicari : "; cin >> judulCari5;
+                cout << "Sesi yang ingin dihapus : "; cin >> cariSesi;
+                deleteSesi_103012400206(L, judulCari5, cariSesi);
+                
+            } else if (pilihan == 7){
+                showAll(L);
+                
+                
+            } else if (pilihan == 8){
+                string judulCari6;
+                cout << "Nama Bootcamp : "; cin >> judulCari6;
+                cout << countSesi_103012400164(L, judulCari6) << "\n";
+                
+                
+            } else if (pilihan == 9){
+                showminMaxSesi_103012400164(L);
+                
+            }
             
         }
     }
