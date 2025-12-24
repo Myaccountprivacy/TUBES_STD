d33cd77b0c27ceb721e71928381f19d56720b99e
derpyzx <62276138+Myaccountprivacy@users.noreply.github.com>
2025-12-24 19:50:47 +0700

    modified:   main.cpp - Minor issue

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
 
     while (pilihan != 10){
         printMenu();
-        cout << "Masukkan pilihan  :";
+        cout << "\nMasukkan pilihan  :";
         cin >> pilihan;
 
         cout << "\n";
